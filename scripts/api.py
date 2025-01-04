3f591e8ae12d0b450e9438455e699fae57e04f5a
train user <train@trainvm.vbo.local>
2025-01-04 16:31:50 +0300

    batch_prediction.py and bist_symbols_with_IS.txt

--- scripts/api.py
+++ scripts/api.py
@@ -51,3 +51,18 @@ def predict(stock_name: str):
         "prediction": f"{prediction:.2f}%",
         "message": "Tahmin basariyla yapildi ve kaydedildi."
     }
+
+@app.post("/batch_predict")
+def batch_predict(stocks: list[str]):
+    results = []
+    for stock_name in stocks:
+        stock_data = df[df['Hisse'] == stock_name]
+        if stock_data.empty:
+            continue
+        input_data = stock_data[selected_features].fillna(0)
+        prediction = model.predict(input_data)[0]
+        stock_name_full = stock_data["Adi"].iloc[0]
+        save_predictions_to_mysql(stock_name, stock_name_full, prediction)
+        log_to_mlflow(stock_name, prediction)
+        results.append({"stock": stock_name, "prediction": prediction})
+    return {"predictions": results, "message": "Tahminler basariyla tamamlandi."}
---
+++ scripts/batch_predict.py
@@ -0,0 +1,14 @@
+import requests
+
+# Hisse isimlerini dosyadan yukle
+with open("/home/train/mlops4/BIST_MLops/data/bist_symbols_with_IS.txt", "r") as file:
+    stocks = [line.strip() for line in file.readlines()]
+
+# API'ye POST istegi gonder
+response = requests.post(
+    "http://localhost:8010/batch_predict",
+    json={"stocks": stocks}
+)
+
+# Sonuclari yazdir
+print(response.json())
